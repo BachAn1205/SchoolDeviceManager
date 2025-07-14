2cdc1fa18b979deb1ee005f6ea5033f4bf1459ff
BachAn1205 <BIT230002@st.cmc-u.edu.vn>
2025-07-14 15:11:29 +0700

    OK All Giao Dien

--- src/main/java/com/example/cuoikyjavaa/config/SecurityConfig.java
+++ src/main/java/com/example/cuoikyjavaa/config/SecurityConfig.java
@@ -11,17 +11,29 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers(
-                    "/index.html", "/login.html", "/register.html", "/dashboard-*.html","/css/**", "/js/**", "/images/**", "/static/**",
-                    "/api/auth/register", "/api/auth/login","/favicon.ico"
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/index.html",
+                                "/login.html",
+                                "/register.html",
+                                "/css/**",
+                                "/js/**",
+                                "/images/**",
+                                "/static/**",
+                                "/admin/**",
+                                "/giangvien/**",
+                                "/kythuatvien/**",
+                                "/nhanvien/**",
+                                "/sinhvien/**",
+                                "/api/auth/register",
+                                "/api/auth/login",
+                                "/favicon.ico"
                         ).permitAll()
-                .anyRequest().authenticated()
-            )
-
-            .logout(logout -> logout.permitAll());
+                        .anyRequest().authenticated()
+                )
+                .logout(logout -> logout.permitAll());
 
         return http.build();
     }
-} 
+}
